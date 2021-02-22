338f450b039a8bea4427f4952323699211898001
Ahmed Said <ahmedsaid89738g@gmail.com>
2021-02-22 14:51:49 +0200

    first commit

---
+++ src/model/classes.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+// Theses classes should correspond to the softmax output of your model.
+
+export const MODEL_CLASSES = {
+  0: "Healthy",
+  1: "Early Blight",
+  2: "Leaf Mold",
+  3: "Bactrial Spot",
+};
